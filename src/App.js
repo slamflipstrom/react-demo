b0e652bf784810674a65656c21d8ad38228069b4
Sam Lindstrom <lindstrom.s@gmail.com>
2017-07-16 15:42:05 -0600

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+import logo from "./gh-icon.png";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="logo" alt="logo" />
+          <h2>Github Profile Viewer</h2>
+        </div>
+        <p>
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
