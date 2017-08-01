0baa3c212b67198e730343ca3245d3504df612e0
Sam Lindstrom <lindstrom.s@gmail.com>
2017-07-31 23:55:10 -0600

    Add React Apollo

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { ApolloClient, ApolloProvider } from "react-apollo";
+
+const client = new ApolloClient();
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />,
+  </ApolloProvider>,
+  document.getElementById("root")
+);
 registerServiceWorker();
